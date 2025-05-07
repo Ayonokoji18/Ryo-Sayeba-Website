835022c7b8fafa68e876694c55f2af84f08fbab0
Pawan Chaudhary <chaudharypawan18@yahoo.com>
2025-05-08 00:11:41 +0530

    everything is updated upto 8 may

--- frontend/src/components/Footer.js
+++ frontend/src/components/Footer.js
@@ -1,7 +1,89 @@
 import React from "react";
+import { MDBFooter, MDBContainer, MDBIcon, MDBBtn } from "mdb-react-ui-kit";
 
 function Footer() {
-  return <h1> Hello this is footer</h1>;
+  return (
+    <MDBFooter className="bg-dark text-center text-white">
+      <MDBContainer className="p-4 pb-0">
+        <section className="mb-4">
+          <MDBBtn
+            outline
+            color="light"
+            floating
+            className="m-1"
+            href="#!"
+            role="button"
+          >
+            <MDBIcon fab icon="fab fa-facebook-f" />
+          </MDBBtn>
+
+          <MDBBtn
+            outline
+            color="light"
+            floating
+            className="m-1"
+            href="#!"
+            role="button"
+          >
+            <MDBIcon fab icon="twitter" />
+          </MDBBtn>
+
+          <MDBBtn
+            outline
+            color="light"
+            floating
+            className="m-1"
+            href="#!"
+            role="button"
+          >
+            <MDBIcon fab icon="google" />
+          </MDBBtn>
+          <MDBBtn
+            outline
+            color="light"
+            floating
+            className="m-1"
+            href="#!"
+            role="button"
+          >
+            <MDBIcon fab icon="instagram" />
+          </MDBBtn>
+
+          <MDBBtn
+            outline
+            color="light"
+            floating
+            className="m-1"
+            href="#!"
+            role="button"
+          >
+            <MDBIcon fab icon="linkedin-in" />
+          </MDBBtn>
+
+          <MDBBtn
+            outline
+            color="light"
+            floating
+            className="m-1"
+            href="#!"
+            role="button"
+          >
+            <MDBIcon fab icon="github" />
+          </MDBBtn>
+        </section>
+      </MDBContainer>
+
+      <div
+        className="text-center p-3"
+        style={{ backgroundColor: "rgba(0, 0, 0, 0.2)" }}
+      >
+        © 2025 Copyright:
+        <a className="text-white" href="/">
+          www.ryosayeba.com
+        </a>
+      </div>
+    </MDBFooter>
+  );
 }
 
 export default Footer;
--- frontend/src/components/Navbars.js
+++ frontend/src/components/Navbars.js
@@ -1,12 +1,5 @@
 import React from "react";
-import {
-  Button,
-  Container,
-  Form,
-  Nav,
-  Navbar,
-  NavDropdown,
-} from "react-bootstrap";
+import { Button, Container, Form, Nav, Navbar } from "react-bootstrap";
 
 function Navbars() {
   return (
@@ -16,6 +9,25 @@ function Navbars() {
           <Navbar.Brand href="/">
             <img src="/images/logo1.png" alt="logo" className="logo-img" />
           </Navbar.Brand>
+          <Navbar.Toggle aria-controls="navbarScroll" />
+          <Navbar.Collapse id="navbarScroll">
+            <Nav>
+              <Nav.Link href="/home"> Home</Nav.Link>
+              <Nav.Link href="/blog"> Blog</Nav.Link>
+              <Nav.Link href="/contact">Contact</Nav.Link>
+              <Nav.Link href="/project">Projects</Nav.Link>
+              <Nav.Link href="/about"> About</Nav.Link>
+            </Nav>
+            <Form className="d-flex">
+              <Form.Control
+                type="search"
+                placeholder="Search"
+                className="me-2"
+                aria-label="Search"
+              />
+              <Button variant="outline-success">Search</Button>
+            </Form>
+          </Navbar.Collapse>
         </Container>
       </Navbar>
     </div>
